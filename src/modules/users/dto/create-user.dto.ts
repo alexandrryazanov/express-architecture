90974fea6e11be4acfce6d06bf463be429f2a5f0
VladimirSazanov <vova.sazanov@mail.ru>
2025-07-14 19:36:25 +0300

    created simple  config version (#3)
    * created simple config version
    
    * fix simple config version
    
    * fix(config): fix return + docker
    
    ---------
    
    Co-authored-by: ryazanov <alexandrscooterryazanov@gmail.com>

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,10 +1,12 @@
 import * as joi from "@hapi/joi";
 import "joi-extract-type";
 
-export const createUserDto = joi.object({
-  name: joi.string().required(),
-  age: joi.number().required(),
-  email: joi.string().email().required(),
-});
+export const createUserDto = joi
+  .object({
+    name: joi.string().required(),
+    age: joi.number().required(),
+    email: joi.string().email().required(),
+  })
+  .required();
 
 export type CreateUserDto = joi.extractType<typeof createUserDto>;
